6991540616eac6edf1329ae4972cb5771852512d
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2023-02-16 20:16:42 +0900

    healthcheck api

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log('hello!')
+import Fastify from 'fastify'
+
+const server = Fastify()
+
+server.get('/healthcheck', async function (request, response) {
+  return { status: 'OK' }
+})
+async function main() {
+  try {
+    await server.listen(3000, '0.0.0.0')
+    console.log('Server ready at http://localhost:3000')
+  } catch (e) {
+    console.error(e)
+    process.exit(1)
+  }
+}
+
+main()
