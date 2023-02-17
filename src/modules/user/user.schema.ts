06163e04ea9beb0a4f14a3d1c8fc0ab2bf6466cd
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2023-02-17 17:46:49 +0900

    }) })Update user schema to include userCore object.

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -1,13 +1,17 @@
 import { z } from 'zod'
 
-const createUserSchema = z.object({
+const userCore = {
   email: z
     .string({
       required_error: 'Email is required',
       invalid_type_error: 'Email must be a string'
     })
     .email(),
-  name: z.string(),
+  name: z.string()
+}
+
+const createUserSchema = z.object({
+  ...userCore,
   password: z.string({
     required_error: 'Email is required',
     invalid_type_error: 'Email must be a string'
