c474d88803a92b07549320a2ac29d7de23be0135
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2023-02-17 13:22:15 +0900

    Add user creation logic to registerUserHandler function

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,19 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { CreatUserInput } from './user.schema'
+import { createUser } from './user.service'
 
 export async function registerUserHandler(
-  request: FastifyRequest,
+  request: FastifyRequest<{
+    Body: CreatUserInput
+  }>,
   reply: FastifyReply
-) {}
+) {
+  const body = request.body
+  try {
+    const user = await createUser(body)
+    return reply.code(201).send(user)
+  } catch (e) {
+    console.error(e)
+    return reply.code(500).send(e)
+  }
+}
