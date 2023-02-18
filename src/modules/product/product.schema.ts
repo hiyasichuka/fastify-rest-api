b9c78257b7665a922a9a308f6ee4341a61737cfd
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2023-02-18 13:55:40 +0900

    Update product schemas and build JSON Schemas for create product, product response, and products response.

--- src/modules/product/product.schema.ts
+++ src/modules/product/product.schema.ts
@@ -6,12 +6,16 @@ const productInput = {
   price: z.number(),
   content: z.string().optional()
 }
+
 const productGenerated = {
   id: z.number(),
   createdAt: z.string(),
   updatedAt: z.string()
 }
-const createProductSchema = z.object({ ...productInput })
+
+const createProductSchema = z.object({
+  ...productInput
+})
 
 const productResponseSchema = z.object({
   ...productInput,
@@ -22,7 +26,7 @@ const productsResponseSchema = z.array(productResponseSchema)
 
 export type CreateProductInput = z.infer<typeof createProductSchema>
 
-export const { schemas: productShemas, $ref } = buildJsonSchemas({
+export const { schemas: productSchemas, $ref } = buildJsonSchemas({
   createProductSchema,
   productResponseSchema,
   productsResponseSchema
