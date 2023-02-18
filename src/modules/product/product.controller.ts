305348c3c10bfade41aa82ea2446923064a2fe6f
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2023-02-18 13:57:07 +0900

    Add async functionality to createProductHandler function.

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -3,9 +3,14 @@ import { CreateProductInput } from './product.schema'
 import { createProduct } from './product.service'
 
 export async function createProductHandler(
-  request: FastifyRequest<{ Body: CreateProductInput }>,
-  reply: FastifyReply
+  request: FastifyRequest<{
+    Body: CreateProductInput
+  }>
 ) {
-  const product = createProduct({ ...request.body, ownerId: request.user.id })
+  const product = await createProduct({
+    ...request.body,
+    ownerId: request.user.id
+  })
+
   return product
 }
